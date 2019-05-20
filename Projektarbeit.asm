09b67ce3a5a6fe76ee1334ad2c87bfee7e82b70f
Jonny <littlebigjonny06@gmail.com>
2019-05-20 09:38:52 +0200

    Introduce variable names for out- and inputs

--- Projektarbeit.asm
+++ Projektarbeit.asm
@@ -1,4 +1,11 @@
 
+DIGIT_0 EQU 0x10
+DIGIT_1 EQU 0x11
+DIGIT_2 EQU 0x12
+DIGIT_3 EQU 0x13
+
+INCREMENT_BUTTON EQU P0.7
+
 org 0x0
 jmp init
 
@@ -10,10 +17,10 @@ MOV P3,#0x3F
 
 loop:
 if_increment_pressed:
-JNB P0.7, end_if_increment_pressed
+JNB INCREMENT_BUTTON, end_if_increment_pressed
 ACALL increment
 while_increment_pressed:
-JB P0.7, while_increment_pressed
+JB INCREMENT_BUTTON, while_increment_pressed
 end_if_increment_pressed:
 JMP loop
 
@@ -24,68 +31,68 @@ RET
 
 
 loopinc0:
-inc 0x10
+inc DIGIT_0
 jmp display0
 
 loopinc1:
-inc 0x11
+inc DIGIT_1
 jmp display1
 
 loopinc2:
-inc 0x12
+inc DIGIT_2
 jmp display2
 
 loopinc3:
-inc 0x13
+inc DIGIT_3
 jmp display3
 
 inc0:
-mov R3, 0x10
-cjne R3 ,#0x09,loopinc0
+mov R3, DIGIT_0
+cjne R3, #0x09, loopinc0
 jmp inc1
 
 inc1:
-mov 0x10,#0x00
-mov R4, 0x11
-cjne R4 ,#0x09,loopinc1
+mov DIGIT_0, #0x00
+mov R4, DIGIT_1
+cjne R4, #0x09, loopinc1
 jmp inc2
 
 inc2:
-mov 0x11,#0x00
-mov R4, 0x12
-cjne R4 ,#0x09,loopinc2
+mov DIGIT_1, #0x00
+mov R4, DIGIT_2
+cjne R4, #0x09, loopinc2
 jmp inc3
 
 inc3:
-mov 0x12,#0x00
-mov R4, 0x13
-cjne R4 ,#0x09,loopinc3
+mov DIGIT_2, #0x00
+mov R4, DIGIT_3
+cjne R4, #0x09, loopinc3
 jmp res
 
 res:
-mov 0x13,#0x00
+mov DIGIT_3, #0x00
 jmp display3
 
 display3:
-MOV A, 0x13
+MOV A, DIGIT_3
 MOV DPTR, #display_codes
 MOVC A,@A+DPTR
 MOV P3, A
 
 display2:
-MOV A, 0x12
+MOV A, DIGIT_2
 MOV DPTR, #display_codes
 MOVC A, @A+DPTR
 MOV P2, A
 
 display1:
-MOV A, 0x11
+MOV A, DIGIT_1
 MOV DPTR, #display_codes
 MOVC A, @A+DPTR
 MOV P1, A
 
 display0:
-MOV A, 0x10
+MOV A, DIGIT_0
 MOV DPTR, #display_codes
 MOVC A, @A+DPTR
 MOV P0, A
